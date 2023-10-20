c87b31437f2c1796449e59e76e49174dcf6a250a
GemotiveerdeStudent <129509756+GemotiveerdeStudent@users.noreply.github.com>
2023-10-20 12:21:39 +0200

    Adding teamviews

--- Models/ViewModels/GameViewModel.cs
+++ Models/ViewModels/GameViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace MatchDayAnalyzerFinal.Models.ViewModels
+﻿using MatchDayAnalyzerFinal.Models.ClassModels;
+using System.ComponentModel.DataAnnotations;
+
+namespace MatchDayAnalyzerFinal.Models.ViewModels
 {
     public class GameViewModel
     {
+        public int Id { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public string? OpponentTeam { get; set; }
+        public int? HomeTeamScore { get; set; }
+        public int? AwayTeamScore { get; set; }
+        public virtual ICollection<Team>? TeamsPlayedGame { get; set; }
+        public virtual ICollection<AttendanceSheet>? AttendanceSheets { get; set; }
     }
 }
--- Models/ViewModels/TeamViewModel.cs
+++ Models/ViewModels/TeamViewModel.cs
@@ -1,7 +1,17 @@
 ﻿
+using MatchDayAnalyzerFinal.Models.ClassModels;
+using System.ComponentModel.DataAnnotations;
+
 namespace MatchDayAnalyzerFinal.Models.ViewModels
 {
     public class TeamViewModel
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public Season? Season { get; set; }
+        public int? SeasonId { get; set; }
+        public virtual ICollection<Player>? PlayersInTeam { get; set; }
+        public virtual ICollection<Game>? Games { get; set; }
+        public byte? TeamPicture { get; set; }
     }
 }
